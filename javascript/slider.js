fecd569e1aea68bb31adc39372392f24d814a275
Lalit Kumar <46291895+lalitkashyapkumar@users.noreply.github.com>
2020-04-19 21:00:24 +0530

    Add files via upload

---
+++ javascript/slider.js
@@ -0,0 +1,17 @@
+    $(document).ready(function() {
+     
+      $("#owl-demo").owlCarousel({
+          autoplay: 3000,
+     
+          // "singleItem:true" is a shortcut for:
+          items : 1, 
+          itemsDesktop : true,
+          itemsDesktopSmall : true,
+          itemsTablet: true,
+          itemsMobile : true
+     
+      });
+     
+    });
+
+
